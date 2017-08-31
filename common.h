9c64891b287b8f56b4896eb6e3232861b3b18304
wangyu- <wangyucn@gmail.com>
2017-08-31 10:59:40 -0500

    turn up conv_timeout and server_conn_timeout

--- common.h
+++ common.h
@@ -86,14 +86,14 @@ const u32_t heartbeat_interval=1000;
 
 const u32_t timer_interval=400;//this should be smaller than heartbeat_interval and retry interval;
 
-//const uint32_t conv_timeout=120000; //120 second
-const u32_t conv_timeout=30000; //for test
+const uint32_t conv_timeout=120000; //120 second
+//const u32_t conv_timeout=30000; //for test
 
 const u32_t client_conn_timeout=10000;
 const u32_t client_conn_uplink_timeout=client_conn_timeout+2000;
 
-//const uint32_t server_conn_timeout=conv_timeout+60000;//this should be 60s+ longer than conv_timeout,so that conv_manager can destruct convs gradually,to avoid latency glicth
-const u32_t server_conn_timeout=conv_timeout+10000;//for test
+const uint32_t server_conn_timeout=conv_timeout+60000;//this should be 60s+ longer than conv_timeout,so that conv_manager can destruct convs gradually,to avoid latency glicth
+//const u32_t server_conn_timeout=conv_timeout+10000;//for test
 
 //const u32_t iptables_rule_keep_interval=4000;
 
