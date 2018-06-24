d3cbbe80851477cf2f85256d402faf038bac6c26
wangyu- <wangyucn@gmail.com>
2018-06-24 07:58:19 -0500

    implement hkdf

--- lib/pbkdf2-sha256.h
+++ lib/pbkdf2-sha256.h
@@ -9,5 +9,20 @@ void PKCS5_PBKDF2_HMAC_SHA256(unsigned char *password, size_t plen,
     const unsigned long iteration_count, const unsigned long key_length,
     unsigned char *output);
 
-void sha2( const unsigned char *input, size_t ilen,
-           unsigned char output[32], int is224 );
+//void sha2( const unsigned char *input, size_t ilen,unsigned char output[32], int is224 );
+
+int hkdf_sha256_extract(
+                          const unsigned char *salt, size_t salt_len,
+                          const unsigned char *ikm, size_t ikm_len,
+                          unsigned char *prk );
+
+int hkdf_sha256_expand( const unsigned char *prk,
+                         size_t prk_len, const unsigned char *info,
+                         size_t info_len, unsigned char *okm, size_t okm_len );
+
+int hkdf_sha256( const unsigned char *salt,
+                  size_t salt_len, const unsigned char *ikm, size_t ikm_len,
+                  const unsigned char *info, size_t info_len,
+                  unsigned char *okm, size_t okm_len );
+
+
